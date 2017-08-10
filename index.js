02376f6c2316fcc49c03b418f8c9440fe3941c6d
Benjamin-Coleman <github email address>
2017-08-10 10:38:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+
+function Sandwich(bread, ingredients, name){
+	this.bread = bread;
+	this.name = name;
+	this.ingredients = ingredients
+	this.serve = function(){
+		console.log('this.name')
+	}
+}
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
+
+serve.call(gc)
+serve.apply([gc])
